869912ce5b3a0eb35843f3faf72f90a1c4c1ab7e
huangzehai <chaogeek@foxmail.com>
2019-06-01 18:28:28 +0800

    添加StreamingJob

--- src/main/java/huangzehai/StreamingJob.java
+++ src/main/java/huangzehai/StreamingJob.java
@@ -18,7 +18,14 @@
 
 package huangzehai;
 
+import huangzehai.model.VehicleEvent;
+import huangzehai.sink.ConsoleSink;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 /**
  * Skeleton for a Flink Streaming Job.
@@ -34,31 +41,44 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
  */
 public class StreamingJob {
 
-	public static void main(String[] args) throws Exception {
-		// set up the streaming execution environment
-		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+    public static void main(String[] args) throws Exception {
+        // set up the streaming execution environment
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-		/*
-		 * Here, you can start creating your execution plan for Flink.
-		 *
-		 * Start with getting some data from the environment, like
-		 * 	env.readTextFile(textPath);
-		 *
-		 * then, transform the resulting DataStream<String> using operations
-		 * like
-		 * 	.filter()
-		 * 	.flatMap()
-		 * 	.join()
-		 * 	.coGroup()
-		 *
-		 * and many more.
-		 * Have a look at the programming guide for the Java API:
-		 *
-		 * http://flink.apache.org/docs/latest/apis/streaming/index.html
-		 *
-		 */
+        /*
+         * Here, you can start creating your execution plan for Flink.
+         *
+         * Start with getting some data from the environment, like
+         * 	env.readTextFile(textPath);
+         *
+         * then, transform the resulting DataStream<String> using operations
+         * like
+         * 	.filter()
+         * 	.flatMap()
+         * 	.join()
+         * 	.coGroup()
+         *
+         * and many more.
+         * Have a look at the programming guide for the Java API:
+         *
+         * http://flink.apache.org/docs/latest/apis/streaming/index.html
+         *
+         */
+        DataStreamSource<String> lines = env.socketTextStream("localhost", 9000);
 
-		// execute program
-		env.execute("Flink Streaming Java API Skeleton");
-	}
+        lines.filter(StringUtils::isNotBlank)
+                .map(line -> {
+                    VehicleEvent vehicleEvent = new VehicleEvent();
+                    String[] items = line.split(",");
+                    vehicleEvent.setId(Integer.valueOf(StringUtils.trim(items[0])));
+                    vehicleEvent.setVin(StringUtils.trim(items[1]));
+                    vehicleEvent.setDateTime(LocalDateTime.parse(StringUtils.trim(items[2])));
+                    vehicleEvent.setEvent(StringUtils.trim(items[3]));
+                    vehicleEvent.setAlerts(StringUtils.trim(items[4]));
+                    return vehicleEvent;
+                }).filter(event -> event.getEvent().equalsIgnoreCase("alert"))
+                .addSink(new ConsoleSink<>());
+        // execute program
+        env.execute("Flink Streaming Java API Skeleton");
+    }
 }
